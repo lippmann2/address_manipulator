735be2067a3ff01936ba1cdaedd6360a99b83a10
Mark D Lippmann <mark.lippmann@gmail.com>
2013-06-22 10:35:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddressManipulator::Application.config.secret_token = 'c517c2958ddc418a8122a22abdf6e71d38e2cd12eed1d5af82fc52a42326f3a30927dc977fb26771823950d2355417662bd8a2f25d2228bd9486e0e33b8e17bf'
