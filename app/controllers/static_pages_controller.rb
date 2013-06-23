fed320d138d3dd7d8f52c23a2675a762ae8739c1
Mark D Lippmann <mark.lippmann@gmail.com>
2013-06-23 13:31:42 -0400

    can locally load a sample url

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
+require 'open-uri'
+
 class StaticPagesController < ApplicationController
   def home
+  	#@some_html = "test"
+  	result = JSON.parse(open("http://data.cityofnewyork.us/resource/tj4w-skrs.json?$limit=2").read)
+  	@some_html = result
   end
 
 end
