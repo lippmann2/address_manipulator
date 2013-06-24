241b89d38041b7ef146071bbe8a8613c1c9482cc
Mark D Lippmann <mark.lippmann@gmail.com>
2013-06-24 14:49:08 -0400

    modified home template and timeout code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,14 @@ require 'open-uri'
 
 class StaticPagesController < ApplicationController
   def home
-  	#@some_html = "test"
-  	result = JSON.parse(open("http://data.cityofnewyork.us/resource/tj4w-skrs.json?$limit=2").read)
-  	@some_html = result
+  	begin
+  		result = JSON.parse(open("http://data.cityofnewyork.us/resource/byv3-iir3.json?$limit=1").read)
+	 	@street_address = result[0]["incident_address"]
+  		@zip_code = result[0]["incident_zip"]
+	rescue Timeout::Error => e
+  		@street_address = "<Time out on NYC Open Data side...>"
+  		@zip_code = "<Time out on NYC Open Data side...>"
+	end
   end
 
 end
