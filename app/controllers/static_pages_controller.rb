b69b67ae1cf0bd53b48e2a87e7cbf5029c8f1974
Mark D Lippmann <mark.lippmann@gmail.com>
2013-06-24 14:57:54 -0400

    changed API timeout parameter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,17 @@
 require 'open-uri'
+require 'timeout'
 
 class StaticPagesController < ApplicationController
   def home
   	begin
-  		result = JSON.parse(open("http://data.cityofnewyork.us/resource/byv3-iir3.json?$limit=1").read)
-	 	@street_address = result[0]["incident_address"]
-  		@zip_code = result[0]["incident_zip"]
-	rescue Timeout::Error => e
+  		Timeout::timeout(1){
+  			result = JSON.parse(open("http://data.cityofnewyork.us/resource/byv3-iir3.json?$limit=1").read)
+	 		@street_address = result[0]["incident_address"]
+  			@zip_code = result[0]["incident_zip"]
+  		}
+	rescue Timeout::Error
   		@street_address = "<Time out on NYC Open Data side...>"
   		@zip_code = "<Time out on NYC Open Data side...>"
 	end
   end
-
 end
